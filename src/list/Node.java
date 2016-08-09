e211d93f69c63879e1d137bba79e838c646ebb7e
Yuhang(Steven) Wang <stevenaura@live.com>
2016-08-09 17:37:16 -0500

    add .gitignore

--- src/list/Node.java
+++ src/list/Node.java
@@ -1,4 +1,5 @@
-public class Node<Item>{
-    Item content;
-    Node next;
+public class Node<G>{
+    G data;
+    Node<G> next;
+    Node(G data1, Node<G> next1) { data = data1; next = next1;}
 }
--- src/list/Test.java
+++ src/list/Test.java
@@ -1,9 +1,7 @@
 public class Test {
     public static void main(String args[])
     {
-        Node n = new Node<int>();
-        n.content = 1;
-        n.next = Null;
-
+        Node n = new Node<String>("a101", null);
+        System.out.println(n.data);
     }
 }
